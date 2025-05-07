54c5fb14271c000ff0db502de0732e5f741198b6
Nerya <serinerya@gmail.com>
2025-05-07 21:29:10 +0300

    p

--- ex/7.5.25/api/index.js
+++ ex/7.5.25/api/index.js
@@ -1,8 +1,17 @@
 const express = require('express')
+const cors = require('cors')
+const bodyParser = require('body-parser')
+
 const config = require('./config')
 console.log(config)
 
 const app = express()
+app.use(cors())
+app.use(bodyParser.json({ extend: true}))
+app.use((req, res, next) => {
+    console.log(`[${new Date().toISOString()}] ${req.path} `)
+    next()
+})
 
 app.get("/api/health-check", (req, res, next) => {
     console.log(req.query)
@@ -19,12 +28,27 @@ app.get("/api/authenticate",(req, res, next) => {
 })
 
 const users = []
-app.get("/auth/register", (req, res, next) => {
-    const userName = req.query.userName
-    const password = req.query.password
+app.post("/auth/register", (req, res, next) => {
+    const userName = req.body.userName
+    const password = req.body.password
     if(userName && password){
         users.push({userName, password})
         return res.send("User registered successfully")
+    }else{
+        return res.status(400).send("Missing userName or password")
+    }
+})
+
+app.get("/auth/login", (req, res, next) => {
+    const userName = req.query.userName
+    const password = req.query.password
+    if(userName && password){
+        const foundUser = users.find(u => u.userName === userName && u.password === password)
+        if (foundUser){
+            return res.send("User logged in successfully")
+        }else{
+            return res.send("Wrong Username or Password")
+        }
     }else{
         return res.status(400).send("Missing userName or password")
     }
